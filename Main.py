2dc605b67794fa40926b5c2aa1a31a7749b6219b
Lincoln-Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-02-29 17:37:42 +0800

    Added Stl support
    Started implementing support for the STL file format, currently only Stl files in the ascii format work and n0o colouring has been implemented. also added .stl test models

--- Main.py
+++ Main.py
@@ -206,80 +206,105 @@ class object(gl):
     def __init__(self, _model, x = 0, y = 0 , z = 0, ax = 0, ay = 0, az = 0, scale_x = 1, scale_y = 1, scale_z = 1,colour = None):
         ax, ay, az = math.radians(ax), math.radians(ay), math.radians(az)
         model = open(_model, 'rt')
-        count = 0
         self.object_array = []
-        current_material = None
-        materials = load_mtl(_model[:-3]+ 'mtl')
-        for line in model:
-            if line == '\n':
-                pass
-            elif line[0] == 'v' and line[1] == ' ':
-                count+=1
-                split = line.split()
-                if len(split[1]) >1:
-                    if split[1][0] == '-' and split[1][1] == '0' and len(split[1]) == 2:
-                        split[1] = '0'
-                    else:
-                        while split[1][-1] == '0' or split[1][-1] == '.':
-                            if split[1][-1] == '.':
-                                split[1] = split[1][:-1]
-                                break
-                            else:
-                                split[1] = split[1][:-1]
-                if len(split[2]) >1:
-                    if split[2][0] == '-' and split[2][1] == '0' and len(split[2]) == 2:
-                        split[2] = '0'
-                    else:
-                        while split[2][-1] == '0' or split[2][-1] == '.':
-                            if split[2][-1] == '.':
-                                split[2] = split[2][:-1]
-                                break
-                            else:
-                                split[2] = split[2][:-1]
-                if len(split[3]) >1:
-                    if split[3][0] == '-' and split[3][1] == '0' and len(split[3]) == 2:
-                        split[3] = '0'
-                    else:
-                        while split[3][-1] == '0' or split[3][-1] == '.':
-                            if split[3][-1] == '.':
-                                split[3] = split[3][:-1]
-                                break
-                            else:
-                                split[3] = split[3][:-1]
-                split[1], split[2], split[3] = rotate_point(float(split[1]), float(split[2]), float(split[3]), az)
-                split[1], split[2], split[3] = rotate_point(float(split[3]), float(split[1]), float(split[2]), ay)
-                split[1], split[2], split[3] = rotate_point(float(split[2]), float(split[3]), float(split[1]), ax)
-                locals()["v"+str(count)] = [float(split[1]*scale_x) + x, float(split[2]*scale_y) - y, float(split[3]*scale_z) + z]
-            elif line[0] == 'f' and line[1] == ' ':
-                split = line.split()
-                for i in range(0, len(split)):
-                    pos = 0
-                    v, vt, vn = '', '', ''
-                    for q in range(0, len(split[i])):
+        if _model[-4:] == '.obj':
+            count = 0
+            current_material = None
+            materials = load_mtl(_model[:-3]+ 'mtl')
+            for line in model:
+                if line == '\n':
+                    pass
+                elif line[0] == 'v' and line[1] == ' ':
+                    count+=1
+                    split = line.split()
+                    if len(split[1]) >1:
+                        if split[1][0] == '-' and split[1][1] == '0' and len(split[1]) == 2:
+                            split[1] = '0'
+                        else:
+                            while split[1][-1] == '0' or split[1][-1] == '.':
+                                if split[1][-1] == '.':
+                                    split[1] = split[1][:-1]
+                                    break
+                                else:
+                                    split[1] = split[1][:-1]
+                    if len(split[2]) >1:
+                        if split[2][0] == '-' and split[2][1] == '0' and len(split[2]) == 2:
+                            split[2] = '0'
+                        else:
+                            while split[2][-1] == '0' or split[2][-1] == '.':
+                                if split[2][-1] == '.':
+                                    split[2] = split[2][:-1]
+                                    break
+                                else:
+                                    split[2] = split[2][:-1]
+                    if len(split[3]) >1:
+                        if split[3][0] == '-' and split[3][1] == '0' and len(split[3]) == 2:
+                            split[3] = '0'
+                        else:
+                            while split[3][-1] == '0' or split[3][-1] == '.':
+                                if split[3][-1] == '.':
+                                    split[3] = split[3][:-1]
+                                    break
+                                else:
+                                    split[3] = split[3][:-1]
+                    split[1], split[2], split[3] = rotate_point(float(split[1]), float(split[2]), float(split[3]), az)
+                    split[1], split[2], split[3] = rotate_point(float(split[3]), float(split[1]), float(split[2]), ay)
+                    split[1], split[2], split[3] = rotate_point(float(split[2]), float(split[3]), float(split[1]), ax)
+                    locals()["v"+str(count)] = [float(split[1]*scale_x) + x, float(split[2]*scale_y) - y, float(split[3]*scale_z) + z]
+                elif line[0] == 'f' and line[1] == ' ':
+                    split = line.split()
+                    for i in range(0, len(split)):
+                        pos = 0
+                        v, vt, vn = '', '', ''
+                        for q in range(0, len(split[i])):
+                            if i != 0:
+                                char = split[i][q]
+                                if char == '/':
+                                    pos += 1
+                                else:
+                                    if pos == 0:
+                                        v += char
+                                    elif pos == 1:
+                                        vt += char
+                                    elif pos == 2:
+                                        vn += char
                         if i != 0:
-                            char = split[i][q]
-                            if char == '/':
-                                pos += 1
-                            else:
-                                if pos == 0:
-                                    v += char
-                                elif pos == 1:
-                                    vt += char
-                                elif pos == 2:
-                                    vn += char
-                    if i != 0:
-                        locals()["fv" + str(i)] = locals()["v"+str(v)]
-                if colour == None and current_material == None:
-                    new_colour = random_colour()
-                else:
-                    if colour != None:
-                        new_colour = colour
+                            locals()["fv" + str(i)] = locals()["v"+str(v)]
+                    if colour == None and current_material == None:
+                        new_colour = random_colour()
                     else:
-                        new_colour = current_material[0]
-                for p in range(2,len(split)-1):
-                    self.object_array.append([locals()["fv1"], locals()["fv"+str(p)], locals()["fv"+str(p+1)], new_colour])
-            elif line.split()[0] == 'usemtl':
-                current_material = materials[line.split()[1]]
+                        if colour != None:
+                            new_colour = colour
+                        else:
+                            new_colour = current_material[0]
+                    for p in range(2,len(split)-1):
+                        self.object_array.append([locals()["fv1"], locals()["fv"+str(p)], locals()["fv"+str(p+1)], new_colour])
+                elif line.split()[0] == 'usemtl':
+                    current_material = materials[line.split()[1]]
+        elif _model[-4:] == '.stl':
+                if model.read()[:5] == 'solid':
+                    ascii_stl = True
+                else:
+                    ascii_stl = False
+                face = []
+                if ascii_stl:
+                    model = open(_model, 'rt')
+                    for line in model:
+                        split = line.split()
+                        if split[0] == 'outer':
+                            face = []
+                        if split[0] == 'vertex':
+                            split[1], split[2], split[3] = rotate_point(float(split[1]), float(split[2]), float(split[3]), az)
+                            split[1], split[2], split[3] = rotate_point(float(split[3]), float(split[1]), float(split[2]), ay)
+                            split[1], split[2], split[3] = rotate_point(float(split[2]), float(split[3]), float(split[1]), ax)
+                            face.append([float(split[1]*scale_x) + x, float(split[2]*scale_y) - y, float(split[3]*scale_z) + z])
+                        if split[0] == 'endloop':
+                            face.append(random_colour())
+                            self.object_array.append(face)
+                            face = []
+                        
+                        
+
         super().map_array.append(self.object_array)
         self.map_position = super().map_array.index(self.object_array)
 
