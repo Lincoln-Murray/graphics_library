32f651e8d03d06a2c45cce9ed2883e895b864f81
Lincoln-Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2024-02-29 17:41:27 +0800

    Update cube_demo.py
    made sphericon the object in the demo and attempted to solve some of the lag issues via using the time library to even out the frame times.

--- cube_demo.py
+++ cube_demo.py
@@ -1,15 +1,12 @@
 import Main
-import math
 from tkinter.ttk import *
 from tkinter import *
+import time
 
-model = 'models/teapot.obj'
-model_2 = 'models/cube.obj'
+model = 'models/Sphericon.stl'
 
 renderer = Main.gl(1920,1080, 55, 1000, 0.1)
-teapot_1 = Main.object(model,-4.5,-1,0,0,45,180, 1, 1, 1, '#0000ff')
-teapot_2 = Main.object(model,4.5,-1,0,0,315,180, 1, 1, 1, '#ff0000')
-cube = Main.object(model_2,0,0,0,0,35,0, 1, 1, 1, None)
+cube = Main.object(model,0,0,0,0,35,0, 0.002, 0.002, 0.002, None)
 
 hwstr = str(renderer.width) + 'x' + str(renderer.height)
 ar = int(renderer.height)/int(renderer.width)
@@ -27,16 +24,21 @@ frame.pack(anchor=SW, side=LEFT)
 viewport = Canvas(frame, width=renderer.width, height=renderer.height)
 viewport.pack(side=TOP)
 
-renderer.view_style(False, 1, 0, 1, '', 0.2, '#000000')
+renderer.view_style(False, 1.5, 1.5, 1.5, '', 0, '',)
 
 def loop():
+    start = time.time()
     viewport.create_rectangle(0,0,renderer.width,renderer.height, fill=renderer.background_colour)
     triangles = renderer.new_frame()
 
-    renderer.move_camera(_camera_angle_z = 0, _camera_angle_y=2)
+    renderer.move_camera(_camera_angle_z = 1, _camera_angle_y=2)
     for tri in triangles:
         viewport.create_polygon([tri[0], tri[1], tri[2], tri[3], tri[4], tri[5]],outline=tri[-2], fill=tri[-1])
-    master.after(5,loop)
+    frame_time = int((time.time() - start)*1000)
+    if frame_time < 50:
+        master.after(50-frame_time,loop)
+    else:
+        master.after(1,loop)
 
 master.after(1,loop)
 master.mainloop()
