300d84d32446fbc9f3d325d9c653fad525e0d1dd
CaptainDeathead <unstableplazma@gmail.com>
2024-05-05 16:50:07 +0800

    Reduced calls to locals() increasing fps by a considerable ammount

--- cube_demo.py
+++ cube_demo.py
@@ -5,16 +5,18 @@ from tkinter import *
 import time
 
 #init the models and library class instances
-model = 'models/cube.stl'
+model = 'models/teapot.obj'
 model_2 = 'models/cube.obj'
 
 renderer = Main.gl(1920,1080, 55, 1000, 0.1)
-cube = Main.object(renderer, model,0,0,1,0,35,0, 0.0000001, 0.0000001, 0.0000001, None)
-cube = Main.object(renderer, model_2,-3,0,1,0,35,0, 0.5, 0.5, 0.5, None)
+teapot_1 = Main.object(renderer, model,-4.5,-1,0,0,45,180, 1, 1, 1, '#FF0000')
+teapot_2 = Main.object(renderer, model,4.5,-1,0,0,315,180, 1, 1, 1, '#0000FF')
+cube = Main.object(renderer, model_2,0,0,0,0,35,0, 1, 1, 1, '#00FF00')
+light1 = Main.light(renderer, 0, 0, 10, 1, 1, 1)
 
 renderer.camera_absolute(_camera_x = 0, _camera_y = 0, _camera_z = 10, _camera_angle_x = 0, _camera_angle_y = 0, _camera_angle_z = 0)
 
-light1 = Main.light(renderer, -10,-10,-10, 1,1,1)
+#light1 = Main.light(renderer, -10,-10,-10, 1,1,1)
 
 #init the window and viewport
 master = Tk()
