1f3f4aadd9acbae00c2527a880792cb243a77261
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2026-01-29 09:05:59 +0800

    Create bmp_output_demo.py

---
+++ bmp_output_demo.py
@@ -0,0 +1,53 @@
+#library imports
+import Main
+import rasteriser
+
+#init the models and library class instances
+model = 'models/cube.stl'
+model_2 = 'models/cube.obj'
+
+renderer = Main.gl(1920,1080, 55, 1000, 0.1)
+cube = Main.object(renderer, model,0,0,1,0,35,0, 0.0000001, 0.0000001, 0.0000001, None)
+cube = Main.object(renderer, model_2,-3,0,1,0,35,0, 0.5, 0.5, 0.5, None)
+
+renderer.camera_absolute(_camera_x = 0, _camera_y = 0, _camera_z = 10, _camera_angle_x = 0, _camera_angle_y = 0, _camera_angle_z = 0)
+
+light1 = Main.light(renderer, -10,-10,-10, 1,1,1)
+
+renderer.view_style(False, 0.1, '')
+
+rast = rasteriser.rasteriser(1920, 1080)
+triangles = renderer.new_frame()
+for tri in triangles:
+    print(tri)
+    rast.draw_triangle(triangle=[tri[0], tri[1], tri[2], tri[3], tri[4], tri[5]], colour=tri[-1])
+
+with open("text.bmp", "wb") as test:
+    string = '424D'
+    size = 1920*1080*4+54
+    print(size)
+    hsize = size.to_bytes(4,'big')
+    print(hsize)
+    hsize = hsize[1:] + hsize[0].to_bytes()
+    for byte in hsize: print(byte)
+    string = bytearray.fromhex(string)
+    for byte in hsize: string.append(byte)
+    for byte in bytearray.fromhex('000000003600000028000000'): string.append(byte)
+    for byte in int(1920).to_bytes(4,'big'): string.append(byte)
+    for byte in int(1080).to_bytes(4,'big'): string.append(byte)
+    for byte in bytearray.fromhex('0100180000000000'): string.append(byte)
+    for byte in int(1920*1080*4).to_bytes(4,'big'): string.append(byte)
+    for byte in bytearray.fromhex('130B0000'): string.append(byte)
+    for byte in bytearray.fromhex('130B0000'): string.append(byte)
+    for byte in bytearray.fromhex('00000000'): string.append(byte)
+    for byte in bytearray.fromhex('00000000'): string.append(byte)
+    count = 0
+    for byte in string:
+        test.write(byte.to_bytes())
+    for x in rast.image_pixels:
+        for y in x:
+            count += 1
+            for byte in bytearray.fromhex(y[1:]): test.write(byte.to_bytes())
+            if count == 2:
+                count = 0
+                for byte in bytearray.fromhex('0000'): test.write(byte.to_bytes())
