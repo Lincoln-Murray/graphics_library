033429398da8d64ed467ff612064b66acb4bfb3e
Lincoln Murray <134500266+Lincoln-Murray@users.noreply.github.com>
2026-01-29 09:05:55 +0800

    Create rasteriser.py

---
+++ rasteriser.py
@@ -0,0 +1,76 @@
+class rasteriser:
+    def __init__(self, _height, _width):
+        self.height = _height
+        self.width = _width
+        self.image_pixels = []
+        for x in range(0, self.height):
+            self.image_pixels.append([])
+            for y in range(0, self.width):
+                self.image_pixels[x].append([])
+                self.image_pixels[x][y] = '#000000'
+    
+    def bottom_triangle(self, triangle, colour):
+        if triangle[1] == triangle[3]:
+            topv = [triangle[4], triangle[5]]
+            v1 = [triangle[0], triangle[1]]
+            v2 = [triangle[2], triangle[3]]
+        elif triangle[3] == triangle[5]:
+            topv = [triangle[0], triangle[1]]
+            v1 = [triangle[4], triangle[5]]
+            v2 = [triangle[2], triangle[3]]
+        else:
+            topv = [triangle[2], triangle[3]]
+            v1 = [triangle[0], triangle[1]]
+            v2 = [triangle[4], triangle[5]]
+        islope1 = (v1[0]-topv[0])/(v1[1]-topv[1])
+        islope2 = (v2[0]-topv[0])/(v2[1]-topv[1])
+
+        x1 = topv[0]
+        x2 = topv[0]
+
+        for y in range(v1[1], topv[1]):
+            for x in range(int(x1), int(x2)):
+                if x > 0 and x < self.width:
+                    self.image_pixels[x][y] = colour
+            x1 += islope1
+            x2 += islope2
+
+    def top_triangle(self, triangle, colour):
+        if triangle[1] == triangle[3]:
+            bottomv = [triangle[4], triangle[5]]
+            v1 = [triangle[0], triangle[1]]
+            v2 = [triangle[2], triangle[3]]
+        elif triangle[3] == triangle[5]:
+            bottomv = [triangle[0], triangle[1]]
+            v1 = [triangle[4], triangle[5]]
+            v2 = [triangle[2], triangle[3]]
+        else:
+            bottomv = [triangle[2], triangle[3]]
+            v1 = [triangle[0], triangle[1]]
+            v2 = [triangle[4], triangle[5]]
+        islope1 = (bottomv[0]-v1[0])/(bottomv[1]-v1[1])
+        islope2 = (bottomv[0]-v2[0])/(bottomv[1]-v2[1])
+
+        x1 = bottomv[0]
+        x2 = bottomv[0]
+
+        for y in range(v1[1], bottomv[1]):
+            for x in range(int(x1), int(x2)):
+                if x > 0 and x < self.width:
+                    self.image_pixels[x][y] = colour
+            x1 -= islope1
+            x2 -= islope2
+
+    def draw_triangle(self, triangle, colour):
+        if (triangle[1] == triangle[3] and triangle[5] < triangle[3]) or (triangle[3] == triangle[5] and triangle[1] < triangle[5]) or (triangle[1] == triangle[5] and triangle[3] < triangle[5]):
+            self.bottom_triangle(triangle, colour)
+        elif (triangle[1] == triangle[3] and triangle[5] > triangle[3]) or (triangle[3] == triangle[5] and triangle[1] > triangle[5]) or (triangle[1] == triangle[5] and triangle[3] > triangle[5]):
+            self.top_triangle(triangle, colour)
+        else:
+            new_tri = [[triangle[0],triangle[1]],[triangle[2],triangle[3]],[triangle[4],triangle[5]]]
+            new_tri.sort(key=lambda y: y[-1])
+            if new_tri[0][1] != new_tri[1][1] and new_tri[1][1] != new_tri[2][1] and new_tri[2][1] and new_tri[0][1]:
+                newx = new_tri[0][0] + (new_tri[1][1] - new_tri[0][1])/(new_tri[2][1] - new_tri[0][1]) * (new_tri[2][0] - new_tri[1][1])
+                self.bottom_triangle([new_tri[0][0], new_tri[0][1], new_tri[1][0], new_tri[1][1], newx, new_tri[1][1]], colour)
+                self.top_triangle([new_tri[2][0], new_tri[2][1], new_tri[1][0], new_tri[1][1], newx, new_tri[1][1]], colour)
+           
